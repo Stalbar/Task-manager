6b9618327e798d57f08638f49cd69572970531e1
Stalbar <aleksey.smorshyok@gmail.com>
2023-04-26 22:15:15 +0300

    add user controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { User } = require('../models/models');
+
+class UserController {
+  async registration(req, res, next) {
+
+  }
+
+  async login(req, res, next) {
+
+  }
+
+  async check(req, res, next) {
+
+  }
+}
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
+const router = require('./routes/index');
 const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
 
 const start = async () => {
   try {
