a55dd17745f6a5f7fc9dc4d371e02f77d074a7c6
Stalbar <aleksey.smorshyok@gmail.com>
2023-04-26 22:44:52 +0300

    add task creation

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -2,8 +2,10 @@ const { Task } = require('../models/models');
 
 class TaskController {
   async addTask(req, res, next) {
-
-  }
+    const { title, content, expiredAt, status, userId } = req.body;
+    const task = await Task.create({ title, content, expiredAt, status, userId});
+    return res.json({task});
+  } 
 
   async getTasks(req, res, next) {
 
