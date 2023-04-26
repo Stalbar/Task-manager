14b0a353d80fb78a80a7deb2ad9c63561d438cda
Stalbar <aleksey.smorshyok@gmail.com>
2023-04-26 22:25:40 +0300

    add login for user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -18,7 +18,29 @@ class UserController {
   }
 
   async login(req, res, next) {
-
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({message: "Email and password are required!"});
+    }
+    const candidate = await User.findOne({where: {email}});
+    if (!candidate) {
+      return res.status(404).json({message: "User not found"});
+    }
+    const match = await bcrypt.compare(password, candidate.password);
+    if (!match) {
+      return res.status(401).json({message: "Invalid password"});
+    }
+    const accessToken = jwt.sign(
+      {
+        "id": candidate.id,
+        "email": candidate.email
+      },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "2h"
+      }
+    )
+    return res.status(200).json({accessToken});
   }
 
   async check(req, res, next) {
