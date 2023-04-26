3e628c17de2b9308819f4bca35728821fce9d7d6
Stalbar <aleksey.smorshyok@gmail.com>
2023-04-27 00:19:35 +0300

    add task modification

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -26,7 +26,18 @@ class TaskController {
   }
 
   async editTask(req, res, next) {
-
+    const { title, content, expiredAt } = req.body;
+    const id = req.params.id;
+    const candidate = await Task.findOne({where: {id}});
+    if (!candidate) {
+      return res.sendStatus(404);
+    }
+    candidate.title = title;
+    candidate.content = content;
+    candidate.expiredAt = expiredAt;
+    candidate.status = new Date().toISOString().slice(0, 10) > expiredAt ? "FAILED" : "IN PROGRESS";
+    await candidate.save();
+    res.status(200).json(candidate);
   }
 }
 
