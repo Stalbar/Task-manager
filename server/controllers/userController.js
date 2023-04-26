19f3c372afd59481d7b60b698d941f9619eb1db6
Stalbar <aleksey.smorshyok@gmail.com>
2023-04-26 22:22:07 +0300

    add registration for user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,17 @@ const { User } = require('../models/models');
 
 class UserController {
   async registration(req, res, next) {
-
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({message: "Email and password are required"});
+    }
+    const candidate = await User.findOne({where: {email}});
+    if (candidate) {
+      return res.status(409).json({message: "User already exists"});
+    }
+    const hashPassword = await bcrypt.hash(password, 5);
+    await User.create({email, password: hashPassword });
+    return res.status(201).json({message: "User created"});
   }
 
   async login(req, res, next) {
